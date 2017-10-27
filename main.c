f6c7bd026ec770d7564d039f9de856737745ad0f
aollivie <aollivie@aollivies-Mac.local>
2017-10-27 15:08:58 +0200

    find segment,section

--- main.c
+++ main.c
@@ -134,71 +134,88 @@ int print_text_text_section(void *ptr, long double addr, int size)
 		ft_print_adress(*(unsigned char *)ptr);
 		write(1, "  ", 2);
 		j++;
-		if (j == 21) {
-			printf("\n");
+		if (j == 21)
+		{
+			write(1, "\n", 1);
 			return (0);
 		}
 		if (j % 16 == 0)
 		{
-			printf("\n");
+			write(1, "\n", 1);
 		}
 		addr++;
 		ptr++;
-
  	}
 	return (EXIT_SUCCESS);
 }
 
-struct section_64 *ft_find_section_64(char *ptr)
+struct section_64 *ft_find_section_64(
+	char *ptr, struct segment_command_64 *segment, char *section_name)
 {
-	struct load_command		*lc;
-	struct segment_command_64   *seg;
-	struct section_64 *section;
+	struct section_64			*section;
+	int							loop;
 
-
+	loop = 0;
+	section = (void *)segment + sizeof(*segment);
+	while (loop < segment->nsects)
+	{
+		if (!strcmp(section->sectname, section_name))
+		{
+			return (section);
+		}
+		section = (void *)section + sizeof(*section);
+		loop++;
+	}
 	return (NULL);
+}
+
+struct segment_command_64 *ft_find_segment_64(
+	char *ptr, struct load_command *lc, int ncmds, char *segment_name)
+{
+	struct segment_command_64	*seg;
+	int							i;
+
+	i = 0;
+	while (i < ncmds)
+	{
+	  if (lc->cmd == LC_SEGMENT_64)
+	  {
+		  seg = (struct segment_command_64*)lc;
+		  if (strcmp(seg->segname, segment_name) == 0)
+		  {
+			  return (seg);
+		  }
+	  }
+	  lc = (void *)lc + lc->cmdsize;
+	  i++;
+	}
+	return (NULL);
+}
+
+struct section_64 *ft_find_segment_section_64(
+	char *ptr, struct mach_header_64 *header, char *segment_name, char *section_name)
+{
+	struct load_command			*lc;
+	struct segment_command_64	*seg;
+	struct section_64			*section;
+
+	lc = (void *)ptr + sizeof(*header);
+	if (!(seg = ft_find_segment_64(ptr, lc, header->ncmds, segment_name)))
+		return (NULL);
+	if (!(section = ft_find_section_64(ptr, seg, section_name)))
+		return (NULL);
+	return (section);
 }
 
 int	handle_64_text(char *ptr)
 {
-	int						ncmds;
-	int						i;
-	struct mach_header_64	*header;
-	struct load_command		*lc;
-	struct segment_command_64   *seg;
-	struct section_64 *section;
-
+	struct mach_header_64		*header;
+	struct section_64			*section;
 
 	header = (struct mach_header_64 *)ptr;
-	ncmds = header->ncmds;
-	lc = (void *)ptr + sizeof(struct mach_header_64);
-	for (i = 0; i < ncmds; i++) {
-	  if (lc->cmd == LC_SEGMENT_64)
-	  {
-	       seg = (struct segment_command_64*)lc;
-	       if (strcmp(seg->segname,SEG_TEXT) == 0)
-				 {
-					 int loop = 0;
-					 section = (void *)seg + sizeof(*seg);
-					 while (loop < seg->nsects) {
-					 	if (!strcmp(section->sectname, SECT_TEXT)) {
-					 		print_text_text_section((void*)ptr + section->offset, section->addr, section->size);
-							break;
-					 	}
-						section = (void *)seg + sizeof(*seg);
-						loop++;
-					 }
-
-					return (0);
-
-				 }
-
-	  }
-
-      	lc = (void *)lc + lc->cmdsize;
-
-	}
-
+	if (!(section = ft_find_segment_section_64(ptr, header, SEG_TEXT, SECT_TEXT)))
+		return (EXIT_FAILURE);
+	print_text_text_section((void*)ptr + section->offset, section->addr, section->size);
 	return (EXIT_SUCCESS);
 }
 

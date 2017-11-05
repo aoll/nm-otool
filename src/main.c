4af4d307556b42329818cc081cf736a7601157d0
aollivie <aollivie@aollivies-Mac.local>
2017-11-05 23:07:54 +0100

    norme 42 school

--- src/main.c
+++ src/main.c
@@ -19,32 +19,22 @@ int	main(int ac, char **av) {
 	int			ret;
 
 	if (ac != 2)
-	{
-		fprintf(stderr, "%s\n", "Please give a arg.");
 		return (EXIT_FAILURE);
-	}
 	if ((fd = open(av[1], O_RDONLY)) < 0)
 	{
-		perror("open");
+		ft_putstr("No access to : ");
+		ft_putstr(av[1]);
+		ft_putstr("\n");
 		return (EXIT_FAILURE);
 	}
 	if (fstat(fd, &buf) < 0)
-	{
-		perror("fstat");
 		return (EXIT_FAILURE);
-	}
 	if ((ptr = mmap(
 		0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-	{
-		perror("mmap");
 		return (EXIT_FAILURE);
-	}
 	// ft_nm(ptr);
-	ret = ft_otool(ptr, (void *)ptr + buf.st_size, av[1], 0);
+	ret = ft_otool(ptr, (void *)ptr + buf.st_size, av[1], 1);
 	if (munmap(ptr, buf.st_size) < 0)
-	{
-		perror("munmap");
 		return (EXIT_FAILURE);
-	}
 	return (ret);
 }

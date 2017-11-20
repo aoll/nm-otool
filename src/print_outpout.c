75f5b6e6a9d1420df6eb2133895d2ed47424d19c
aollivie <aollivie@aollivies-Mac.local>
2017-11-20 16:53:15 +0100

    delete usless function

--- src/print_outpout.c
+++ src/print_outpout.c
@@ -12,51 +12,44 @@
 
 #include "ft_otool.h"
 
-int swap_uint32_check_no(int val, int a)
-{
-	if (a)
-		return (val);
-	return (val);
-}
-
 static void	case_n_sect(
-	struct nlist *nlist, t_seg_infos *seg_infos, char *type, t_cmd_flag *cmd_f)
+	struct nlist *nlist, t_seg_infos *seg_infos, char *type)
 {
-	if ((int)swap_uint32_check_no(nlist->n_sect, cmd_f->is_indian) == seg_infos->text_nsect)
+	if (nlist->n_sect == seg_infos->text_nsect)
 		*type = 't';
-	else if ((int)swap_uint32_check_no(nlist->n_sect, cmd_f->is_indian) == seg_infos->data_nsect)
+	else if (nlist->n_sect == seg_infos->data_nsect)
 		*type = 'd';
-	else if ((int)swap_uint32_check_no(nlist->n_sect, cmd_f->is_indian) == seg_infos->bss_nsect)
+	else if (nlist->n_sect == seg_infos->bss_nsect)
 		*type = 'b';
 	else
 		*type = 's';
-	if ((int)swap_uint32_check_no(nlist->n_sect, cmd_f->is_indian) == seg_infos->bss_nsect)
+	if (nlist->n_sect == seg_infos->bss_nsect)
 	{
 		*type = 'b';
 	}
 }
 
-static void	case_n_undef(struct nlist *nlist, char *type, t_cmd_flag *cmd_f)
+static void	case_n_undef(struct nlist *nlist, char *type)
 {
 	*type = 'u';
-	if (swap_uint32_check_no(nlist->n_value, cmd_f->is_indian) != 0)
+	if (nlist->n_value != 0)
 		*type = 'c';
 }
 
 static int	set_type(
-	struct nlist *nlist, t_seg_infos *seg_infos, t_cmd_flag *cmd_f)
+	struct nlist *nlist, t_seg_infos *seg_infos)
 {
 	char				type;
 	int					c;
 
 	type = '?';
-	c = swap_uint32_check_no(nlist->n_type, cmd_f->is_indian) & N_TYPE;
+	c = nlist->n_type & N_TYPE;
 	if (c == N_UNDF)
-		case_n_undef(nlist, &type, cmd_f);
+		case_n_undef(nlist, &type);
 	else if (c == N_ABS)
 		type = 'a';
 	else if (c == N_SECT)
-		case_n_sect(nlist, seg_infos, &type, cmd_f);
+		case_n_sect(nlist, seg_infos, &type);
 	else if (c == N_PBUD)
 		type = 'u';
 	else if (c == N_INDR)
@@ -70,17 +63,17 @@ int			print_outpout(
 {
 	char				type;
 
-	if ((swap_uint32_check_no(nlist->n_type, cmd_f->is_indian) & N_STAB) != 0)
+	if ((nlist->n_type & N_STAB) != 0)
 		return (EXIT_SUCCESS);
-	type = set_type(nlist, seg_infos, cmd_f);
+	type = set_type(nlist, seg_infos);
 	if (cmd_f->u && type != 'u')
 		return (EXIT_SUCCESS);
 	if (cmd_f->uu && type == 'u')
 		return (EXIT_SUCCESS);
 	if (cmd_f->g
-		&& !(swap_uint32_check_no(nlist->n_type, cmd_f->is_indian) & N_EXT))
+		&& !(nlist->n_type & N_EXT))
 		return (EXIT_SUCCESS);
-	if ((swap_uint32_check_no(nlist->n_type, cmd_f->is_indian) & N_EXT)
+	if ((nlist->n_type & N_EXT)
 	&& type != '?')
 		type = ft_toupper(type);
 	print_outpout_format(
